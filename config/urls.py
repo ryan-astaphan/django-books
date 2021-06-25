8ee7f78a23ab6bc9f99e78b398534d04db1cd6dc
ryan-in-thailand <ryanastaphan@gmail.com>
2021-06-25 15:25:29 +0700

    ch16

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path('boss-access/', admin.site.urls),
 
     # User management
     path('accounts/', include('allauth.urls')),
